898b5b7a88035786a2c885e92ca33315ebb3ddbc
Nikunj Lahoti <nikunj.lahoti@gmail.com>
2020-05-29 22:58:07 +0530

    Fixed the Python response to be Json compliant
    Added output variable which executes on `terraform apply`.
    Added Terraform Log to show the output message.
    Python function now returns a JSON which terraform output variable can
    understand.

--- Exercise_2/outputs.tf
+++ Exercise_2/outputs.tf
@@ -1,1 +1,18 @@
-# TODO: Define the output variable for the lambda function.
+# Define the output variable for the lambda function.
+
+# Invoking the Lambda function. Our function doesn't require any input however.
+data "aws_lambda_invocation" "example" {
+  function_name = aws_lambda_function.lambda.function_name
+  input = <<JSON
+{
+  "irrelevant": "sample",
+  "input": "values"
+}
+JSON
+}
+
+# Collecting the output inside a variable which will be printed out during Terraform Apply phase.
+output "Lambda-Execution-Result" {
+  description = "String result of Lambda execution"
+  value       = data.aws_lambda_invocation.example.result
+}
